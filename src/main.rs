c0cfea773381866b522b8a22358fcae32564f925
pimukthee <pimukthee@gmail.com>
2024-06-24 21:24:42 +0700

    add echo

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::io::BufRead;
+use std::io::{self, BufRead, Write};
 
+use anyhow::Context;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -24,12 +25,44 @@ struct Body {
 enum Kind {
     Init(Init),
     InitOk,
+    Echo { echo: String },
+    EchoOk { echo: String },
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct Init {
     node_id: String,
     node_ids: Vec<String>,
+}
+
+impl Kind {
+    fn into_response(self) -> Kind {
+        match self {
+            Kind::Init(_) => Kind::InitOk,
+            Kind::Echo { echo } => Kind::EchoOk { echo },
+            _ => unimplemented!(),
+        }
+    }
+}
+
+impl Message {
+    fn into_response(self, output: &mut impl io::Write) -> anyhow::Result<()> {
+        let response = Message {
+            src: self.dst,
+            dst: self.src,
+            body: Body {
+                msg_id: Some(0),
+                in_reply_to: self.body.msg_id,
+                kind: self.body.kind.into_response(),
+            },
+        };
+        serde_json::to_writer(&mut *output, &response).context("serialize response")?;
+        output
+            .write_all(b"\n")
+            .context("write new line to buffer")?;
+
+        Ok(())
+    }
 }
 
 fn main() -> anyhow::Result<()> {
@@ -39,17 +72,17 @@ fn main() -> anyhow::Result<()> {
     let init_message =
         serde_json::from_str::<Message>(&stdin.next().expect("no init message received")?)?;
 
-    let reply = Message {
-        src: init_message.dst,
-        dst: init_message.src,
-        body: Body {
-            msg_id: Some(0),
-            in_reply_to: init_message.body.msg_id,
-            kind: Kind::InitOk,
-        },
-    };
+    init_message
+        .into_response(&mut stdout)
+        .context("response")?;
 
-    serde_json::to_writer(&mut stdout, &reply);
+    for line in stdin {
+        let message =
+            serde_json::from_str::<Message>(&line.context("Maelstrom input could not be read")?)
+                .context("maelstrom input could not be deserialize")?;
+
+        message.into_response(&mut stdout).context("response")?;
+    }
 
     Ok(())
 }
